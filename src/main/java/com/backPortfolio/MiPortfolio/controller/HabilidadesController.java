b5123c3e6c067cec1ecaaa71544b389cd2f2030a
Luchopaulucci <luchopaluch@gmail.com>
2023-04-02 20:40:40 -0300

    Se cambia de db(a la nueva)

--- src/main/java/com/backPortfolio/MiPortfolio/controller/HabilidadesController.java
+++ src/main/java/com/backPortfolio/MiPortfolio/controller/HabilidadesController.java
@@ -1,11 +1,9 @@
 package com.backPortfolio.MiPortfolio.controller;
 
 import com.backPortfolio.MiPortfolio.entity.EHabilidades;
-import com.backPortfolio.MiPortfolio.service.SExperiencia;
 import com.backPortfolio.MiPortfolio.service.SHabilidades;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +12,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/habilidades")
+@RequestMapping("habilidades/")
 @CrossOrigin(origins = "https://portfolio-frontend-luciano.web.app")
 public class HabilidadesController {
     
